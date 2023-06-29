977f17efeacf958fd81304d08ecae6cbfd0a25d8
anhlan09 <107197832+anhlan09@users.noreply.github.com>
2023-06-29 16:20:58 +0700

    code crud quiz (#12)
    * code crud quiz
    
    * sua lan 1

---
+++ QuizzBankBE/DTOs/BaseDTO/BaseQuizDTO.cs
@@ -0,0 +1,29 @@
+﻿namespace QuizzBankBE.DTOs.BaseDTO
+{
+    public class BaseQuizDTO
+    {
+
+        public int Courseid { get; set; }
+        public string Name { get; set; } = null!;
+
+        public string? Intro { get; set; }
+
+        public DateTime? TimeOpen { get; set; }
+
+        public DateTime? TimeClose { get; set; }
+
+        public string? TimeLimit { get; set; }
+
+        public string? Overduehanding { get; set; }
+
+        public string? PreferedBehavior { get; set; }
+
+        public float PointToPass { get; set; }
+
+        public float MaxPoint { get; set; }
+
+        public string NaveMethod { get; set; } = null!;
+
+        public sbyte IsPublic { get; set; }
+    }
+}
---
+++ QuizzBankBE/Services/QuizService/IQuizService.cs
@@ -0,0 +1,16 @@
+﻿using QuizzBankBE.DTOs;
+using QuizzBankBE.DTOs.BaseDTO;
+using QuizzBankBE.Model;
+using QuizzBankBE.Model.Pagination;
+
+namespace QuizzBankBE.Services.QuizService
+{
+    public interface IQuizService
+    {
+        Task<ServiceResponse<QuizResponseDTO>> createNewQuiz(CreateQuizDTO createQuizDTO);
+        Task<ServiceResponse<PageList<QuizDTO>>> getAllQuiz(OwnerParameter ownerParameters);
+        Task<ServiceResponse<QuizDTO>> deleteQuizz(int id);
+        Task<ServiceResponse<BaseQuizDTO>> updateQuizz(BaseQuizDTO updateQuizDTO, int id);
+
+    }
+}
