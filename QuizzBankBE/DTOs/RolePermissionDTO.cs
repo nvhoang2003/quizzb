3304e9eff0080278b68075f46e0b854ce8b80e14
Huwnga <hungbungbu2003@gmail.com>
2023-07-24 15:17:41 +0700

    isPermission always false

--- QuizzBankBE/DTOs/RolePermissionDTO.cs
+++ QuizzBankBE/DTOs/RolePermissionDTO.cs
@@ -35,6 +35,6 @@ namespace QuizzBankBE.DTOs
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Description { get; set; }
-        public bool isPermission { get; set; }
+        public bool isPermission { get; set; } = false;
     }
 }
