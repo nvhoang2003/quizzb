eb39447b783a74849c4b8c2983fc63a2bcc35979
anhlan09 <lethianhlan2003@gmail.com>
2023-07-24 15:46:01 +0700

    crud_quiz lan 1

--- QuizzBankBE/Controllers/QuizzController.cs
+++ QuizzBankBE/Controllers/QuizzController.cs
@@ -1,100 +1,146 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Cors;
-//using Microsoft.AspNetCore.Mvc;
-//using Newtonsoft.Json;
-//using QuizzBankBE.DataAccessLayer.Data;
-//using QuizzBankBE.DTOs;
-//using QuizzBankBE.Model;
-//using QuizzBankBE.Model.Pagination;
-//using QuizzBankBE.Services.QuizService;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using QuizzBankBE.DataAccessLayer.Data;
+using QuizzBankBE.DTOs;
+using QuizzBankBE.Model;
+using QuizzBankBE.Model.Pagination;
+using QuizzBankBE.Services.QuizService;
+using System.Security.Claims;
 
-//namespace QuizzBankBE.Controllers
-//{
+namespace QuizzBankBE.Controllers
+{
 
-//    [Authorize]
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    [EnableCors("AllowAll")]
-//    [Produces("application/json")]
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+    [Produces("application/json")]
 
-
-//    public class QuizzController : ControllerBase
-//    {
-//        private readonly IQuizService _quizServices;
-//        private readonly IHttpContextAccessor _httpContextAccessor;
-//        private readonly DataContext _dataContext;
-//        private readonly IConfiguration _configuration;
-
-
-//        public QuizzController(IQuizService quizServices, IHttpContextAccessor httpContextAccessor, DataContext dataContext, IConfiguration configuration)
-//        {
-//            _quizServices = quizServices;
-//            _httpContextAccessor = httpContextAccessor;
-//            _dataContext = dataContext;
-//            _configuration = configuration;
-//        }
+    public class QuizzController : ControllerBase
+    {
+        private readonly IQuizService _quizServices;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly DataContext _dataContext;
+        private readonly IConfiguration _configuration;
 
 
-//        [HttpPost("CreateNewQuizz")]
-//        public async Task<ActionResult<ServiceResponse<QuizResponseDTO>>> createNewQizz(
-//        [FromBody] CreateQuizDTO createQuizDTO)
-//        {
-//            var response = await _quizServices.createNewQuiz(createQuizDTO);
+        public QuizzController(IQuizService quizServices, IHttpContextAccessor httpContextAccessor, DataContext dataContext, IConfiguration configuration)
+        {
+            _quizServices = quizServices;
+            _httpContextAccessor = httpContextAccessor;
+            _dataContext = dataContext;
+            _configuration = configuration;
+        }
 
-//            return Ok(response);
-//        }
+        [HttpPost("CreateNewQuizz")]
+        public async Task<ActionResult<ServiceResponse<QuizResponseDTO>>> createNewQizz(
+        [FromBody] CreateQuizDTO createQuizDTO)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permission = (from u in _dataContext.Users
+                              join r in _dataContext.Roles on u.RoleId equals r.Id
+                              join rp in _dataContext.RolePermissions on r.Id equals rp.RoleId
+                              join p in _dataContext.Permissions on rp.PermissionId equals p.Id
+                              where u.Id == userIdLogin
+                              where p.Name == _configuration.GetSection("Permission:WRITE_QUIZZ").Value
+                              select p).FirstOrDefault();
 
+            if (permission == null)
+            {
+                return new StatusCodeResult(403);
+            }
 
-//        [HttpGet("getListAllQuizz")]
-//        public async Task<ActionResult<ServiceResponse<PageList<QuizDTO>>>> getListQuizz(
-//        [FromQuery] OwnerParameter ownerParameters)
-//        {
-//            var response = await _quizServices.getAllQuiz(ownerParameters);
-//            var metadata = new
-//            {
-//                response.Data.TotalCount,
-//                response.Data.PageSize,
-//                response.Data.CurrentPage,
-//                response.Data.TotalPages,
-//                response.Data.HasNext,
-//                response.Data.HasPrevious
-//            };
-//            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+            var response = await _quizServices.createNewQuiz(createQuizDTO);
 
-//            return Ok(response);
-//        }
+            return Ok(response);
+        }
 
+        [HttpGet("getListAllQuizz")]
+        public async Task<ActionResult<ServiceResponse<PageList<QuizDTO>>>> getListQuizz(
+        [FromQuery] OwnerParameter ownerParameters)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permission = (from u in _dataContext.Users
+                              join r in _dataContext.Roles on u.RoleId equals r.Id
+                              join rp in _dataContext.RolePermissions on r.Id equals rp.RoleId
+                              join p in _dataContext.Permissions on rp.PermissionId equals p.Id
+                              where u.Id == userIdLogin
+                              where p.Name == _configuration.GetSection("Permission:READ_QUIZZ").Value
+                              select p).FirstOrDefault();
 
-//        [HttpPut("updateQuiz/{id}")]
-//        public async Task<ActionResult<ServiceResponse<QuizDTO>>> updateQuiz(
-//        [FromBody] CreateQuizDTO updateQuizDTO, int id)
-//        {
+            if (permission == null)
+            {
+                return new StatusCodeResult(403);
+            }
 
-//            var response = await _quizServices.updateQuizz(updateQuizDTO, id);
+            var response = await _quizServices.getAllQuiz(ownerParameters);
+            var metadata = new
+            {
+                response.Data.TotalCount,
+                response.Data.PageSize,
+                response.Data.CurrentPage,
+                response.Data.TotalPages,
+                response.Data.HasNext,
+                response.Data.HasPrevious
+            };
 
-//            return Ok(response);
-//        }
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(metadata));
+            return Ok(response);
+        }
 
-//        [HttpDelete("deleteQuiz/{id}")]
-//        public async Task<ActionResult<ServiceResponse<QuizDTO>>> deleteQuiz(int id)
-//        {
+        [HttpPut("updateQuiz/{id}")]
+        public async Task<ActionResult<ServiceResponse<QuizDTO>>> updateQuiz(
+        [FromBody] CreateQuizDTO updateQuizDTO, int id)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permission = (from u in _dataContext.Users
+                              join r in _dataContext.Roles on u.RoleId equals r.Id
+                              join rp in _dataContext.RolePermissions on r.Id equals rp.RoleId
+                              join p in _dataContext.Permissions on rp.PermissionId equals p.Id
+                              where u.Id == userIdLogin
+                              where p.Name == _configuration.GetSection("Permission:WRITE_QUIZZ").Value
+                              select p).FirstOrDefault();
 
-//            var response = await _quizServices.deleteQuizz(id);
-//            if (response.Status == false)
-//            {
-//                return BadRequest(new ProblemDetails
-//                {
-//                    Status = response.StatusCode,
-//                    Title = response.Message
-//                });
-//            }
+            if (permission == null)
+            {
+                return new StatusCodeResult(403);
+            }
 
-//            return Ok(response);
-//        }
+            var response = await _quizServices.updateQuizz(updateQuizDTO, id);
 
+            return Ok(response);
+        }
 
+        [HttpDelete("deleteQuiz/{id}")]
+        public async Task<ActionResult<ServiceResponse<QuizDTO>>> deleteQuiz(int id)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permission = (from u in _dataContext.Users
+                              join r in _dataContext.Roles on u.RoleId equals r.Id
+                              join rp in _dataContext.RolePermissions on r.Id equals rp.RoleId
+                              join p in _dataContext.Permissions on rp.PermissionId equals p.Id
+                              where u.Id == userIdLogin
+                              where p.Name == _configuration.GetSection("Permission:READ_QUIZZ").Value
+                              select p).FirstOrDefault();
 
+            if (permission == null)
+            {
+                return new StatusCodeResult(403);
+            }
 
-
-//    }
-//}
+            var response = await _quizServices.deleteQuizz(id);
+            if (response.Status == false)
+            {
+                return BadRequest(new ProblemDetails
+                {
+                    Status = response.StatusCode,
+                    Title = response.Message
+                });
+            }
+            
+            return Ok(response);
+        }
+    }
+}
--- QuizzBankBE/DTOs/QuizDTO.cs
+++ QuizzBankBE/DTOs/QuizDTO.cs
@@ -1,34 +1,24 @@
-﻿//using QuizzBankBE.DataAccessLayer.DataObject;
-//using QuizzBankBE.DTOs.BaseDTO;
+﻿using QuizzBankBE.DataAccessLayer.DataObject;
+using QuizzBankBE.DTOs.BaseDTO;
 
-//namespace QuizzBankBE.DTOs
-//{
-//    public class QuizDTO : BaseQuizDTO
-//    {
-//        public int Idquiz { get; set; }
-//        public DateTime? Createdat { get; set; }
+namespace QuizzBankBE.DTOs
+{
+    public class QuizDTO : BaseQuizDTO
+    {
+        public int ID { get; set; }
+        public DateTime? CreateDate { get; set; }
 
-//        public DateTime? Updatedat { get; set; }
-
-//    }
-
-
-
-//    public class QuizResponseDTO
-//    {
-//        public int IdquizVersions { get; set; }
-//        public virtual QuizDTO quiz { get; set; }
-//        public string Status { get; set; }
-//        public int Version { get; set; }
-//        public int? IsDeleted { get; set; }
-//    }
-
-
-//    public class CreateQuizDTO : BaseQuizDTO
-//    {
-
-//    }
-
-   
-
-//}
+        public DateTime? UpdateDate { get; set; }
+    }
+    public class QuizResponseDTO
+    {
+        public int IdquizVersions { get; set; }
+        public virtual QuizDTO quiz { get; set; }
+        public string Status { get; set; }
+        public int Version { get; set; }
+        public int? IsDeleted { get; set; }
+    }
+    public class CreateQuizDTO : BaseQuizDTO
+    {
+    }
+}
