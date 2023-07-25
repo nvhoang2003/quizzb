ccba563259025eb2706139460d72f5cd3c6d92cd
Huwnga <hungbungbu2003@gmail.com>
2023-07-25 14:28:34 +0700

    Rename branch

---
+++ QuizzBankBE/DTOs/QuestionBankMatchingDTO.cs
@@ -0,0 +1,18 @@
+﻿using QuizzBankBE.DataAccessLayer.DataObject;
+using QuizzBankBE.FormValidator;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuizzBankBE.DTOs
+{
+    public class QuestionBankMatchingDTO
+    {
+        [IdExistValidation<Category>("ID")]
+        public int CategoryId { get; set; }
+
+        [Range(0, 1)]
+        public sbyte? IsPublic { get; set; }
+        public virtual ICollection<QuestionBankAnswerDTO> Answers { get; set; }
+
+        public string Questionstype = "Matching";
+    }
+}
