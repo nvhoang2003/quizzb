35cea46a2bef7f3c7b7c564c903e4d0048966d65
nvhoang2003 <nvhoang12082003@gmail.com>
2023-07-26 12:49:13 +0700

    code manage multipe question bank

---
+++ QuizzBankBE/Utility/CheckPermission.cs
@@ -0,0 +1,20 @@
+﻿using QuizzBankBE.DataAccessLayer.Data;
+
+namespace QuizzBankBE.Utility
+{
+    public class CheckPermission
+    {
+        public static bool check(int userIdLogin, string permissionName)
+        {
+            DataContext _dataContext = new DataContext();
+
+            return (from u in _dataContext.Users
+                    join r in _dataContext.Roles on u.RoleId equals r.Id
+                    join rp in _dataContext.RolePermissions on r.Id equals rp.RoleId
+                    join p in _dataContext.Permissions on rp.PermissionId equals p.Id
+                    where u.Id == userIdLogin
+                    where p.Name == permissionName
+                    select p).FirstOrDefault() != null;
+        }
+    }
+}
