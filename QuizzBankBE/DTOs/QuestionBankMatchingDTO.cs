4ca9805e15827cdab6e11df892a6fc9c99aebe5e
Huwnga <hungbungbu2003@gmail.com>
2023-07-26 14:25:13 +0700

    Create Matching Question DTO

--- QuizzBankBE/DTOs/QuestionBankMatchingDTO.cs
+++ QuizzBankBE/DTOs/QuestionBankMatchingDTO.cs
@@ -1,18 +1,32 @@
-﻿using QuizzBankBE.DataAccessLayer.DataObject;
+﻿using Microsoft.AspNetCore.Http;
+using QuizzBankBE.DataAccessLayer.DataObject;
+using QuizzBankBE.DTOs.BaseDTO;
 using QuizzBankBE.FormValidator;
 using System.ComponentModel.DataAnnotations;
 
 namespace QuizzBankBE.DTOs
 {
-    public class QuestionBankMatchingDTO
+    public class CreateQuestionBankMatchingDTO : QuestionDTO
     {
         [IdExistValidation<Category>("ID")]
         public int CategoryId { get; set; }
 
         [Range(0, 1)]
         public sbyte? IsPublic { get; set; }
-        public virtual ICollection<QuestionBankAnswerDTO> Answers { get; set; }
+
+        public virtual ICollection<CreateMatchSubQuestionBankDTO> Answers { get; set; }
 
         public string Questionstype = "Matching";
+    }
+
+    public class CreateMatchSubQuestionBankDTO
+    {
+        [Required]
+        [StringLength(Const.String)]
+        public string? QuestionText { get; set; }
+
+        [Required]
+        [StringLength(Const.String)]
+        public string? AnswerText { get; set; }
     }
 }
