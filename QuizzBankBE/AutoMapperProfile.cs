5e1e68c27b33771e8adf700f7a9a08f4268bae9e
anhlan09 <107197832+anhlan09@users.noreply.github.com>
2023-08-02 16:54:01 +0700

    Crud quizz access (#36)
    * create, updateStatus QuizzAccess
    
    * create

--- QuizzBankBE/AutoMapperProfile.cs
+++ QuizzBankBE/AutoMapperProfile.cs
@@ -95,6 +95,18 @@ namespace QuizzBankBE
             CreateMap<CreateQBankDragAndDropDTO, QuizBank>();
             CreateMap<QuizBank, CreateQBankDragAndDropDTO>();
             CreateMap<QuizBank, QBankDragAndDropDTO>();
+            CreateMap<QuizAccess, CreateQuizAccessDTO>();
+            CreateMap<CreateQuizAccessDTO, QuizAccess>();
+            CreateMap<QuizAccess, BaseQuizAccessDTO>();
+            CreateMap<BaseQuizAccessDTO, QuizAccess>();
+            CreateMap<QuizAccessDTO, QuizAccess>();
+            CreateMap<QuizAccess, QuizAccessDTO>();
+            CreateMap<QuizAccessDTO, BaseQuizAccessDTO>();
+            CreateMap<BaseQuizAccessDTO, QuizAccessDTO>();
+            CreateMap<CreateQuizAccessDTO, BaseQuizAccessDTO>();
+            CreateMap<BaseQuizAccessDTO, CreateQuizAccessDTO>();
+            CreateMap<CreateQuizAccessDTO, QuizAccessDTO>();
+            CreateMap<QuizAccessDTO,CreateQuizAccessDTO>();
         }
     }
 }
---
+++ QuizzBankBE/Controllers/QuizAccessController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QuizzBankBE.DataAccessLayer.Data;
+using QuizzBankBE.DTOs;
+using QuizzBankBE.Model;
+using QuizzBankBE.Services.QuizService;
+using QuizzBankBE.Utility;
+using System.Security.Claims;
+
+namespace QuizzBankBE.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAll")]
+    [Produces("application/json")]
+
+    public class QuizAccessController : ControllerBase
+    {
+        private readonly IQuizzAccessService _quizAccessService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly DataContext _dataContext;
+        private readonly IConfiguration _configuration;
+
+
+        public QuizAccessController(IQuizzAccessService quizzAccess, IHttpContextAccessor httpContextAccessor, DataContext dataContext, IConfiguration configuration)
+        {
+            _quizAccessService = quizzAccess;
+            _httpContextAccessor = httpContextAccessor;
+            _dataContext = dataContext;
+            _configuration = configuration;
+        }
+
+
+        [HttpPost("CreateNewQuizzAccess")]
+        public async Task<ActionResult<ServiceResponse<QuizAccessDTO>>> createNewQizzAccess(
+        [FromBody] CreateQuizAccessDTO createQuizDTO)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permissionName = _configuration.GetSection("Permission:WRITE_QUIZZ").Value;
+
+            if (!CheckPermission.check(userIdLogin, permissionName))
+            {
+                return new StatusCodeResult(403);
+            }
+
+            var response = await _quizAccessService.createQuizzAccess(createQuizDTO);
+
+            return Ok(response);
+        }
+
+
+        [HttpPut("updateStausQuizzAccess/{id}")]
+        public async Task<ActionResult<ServiceResponse<QuizAccessDTO>>> updateStatus(
+              [FromBody] CreateQuizAccessDTO updateQuestionDTO, int id)
+        {
+            var userIdLogin = int.Parse(User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value);
+            var permissionName = _configuration.GetSection("Permission:WRITE_QUIZZ").Value;
+
+            if (!CheckPermission.check(userIdLogin, permissionName))
+            {
+                return new StatusCodeResult(403);
+            }
+
+            var response = await _quizAccessService.updateStatus(updateQuestionDTO, id);
+            return Ok(response);
+        }
+
+    }
+}
